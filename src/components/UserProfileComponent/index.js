a042c6c5bab6626cdab8395df155d7ae415ec741
Idriss Dimson <idriss.dimson@gmail.com>
2019-11-21 15:04:34 -0500

    first commit

---
+++ src/components/UserProfileComponent/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { UserInformation } from "../../components/UserProfileComponent";
+
+export default function UserProfile() {
+    return (
+        <div>
+            <div>User Profile</div>
+            <UserInformation/>
+        </div>
+    );
+}
