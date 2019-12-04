4509e288f6711a77bb2a5200cf96b4eae35ab92f
Idriss Dimson <idriss.dimson@gmail.com>
2019-12-04 01:12:41 -0500

    fixed

--- src/App.js
+++ src/App.js
@@ -10,14 +10,14 @@ import Login from "./pages/Login";
 import UserProfile from "./pages/UserProfile";
 import Signup from "./pages/Signup";
 
-const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECTID,
-  storageBucket: process.env.FIREBASE_STORAGEBUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGINGSENDER,
-  appId: process.env.FIREBASE_APPID
+let firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: "exercise-five-e7ed8.firebaseapp.com",
+  databaseURL: "https://exercise-five-e7ed8.firebaseio.com",
+  projectId: "exercise-five-e7ed8",
+  storageBucket: "exercise-five-e7ed8.appspot.com",
+  messagingSenderId: "806229689552",
+  appId: "1:806229689552:web:ee55aebb11119943b3cad5"
 };
 
 function App() {
