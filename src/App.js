4c394bb7f6bb2eec32f656b8ec1ba65d1969840f
Idriss Dimson <idriss.dimson@gmail.com>
2019-12-04 00:40:20 -0500

    removed stuff

--- src/App.js
+++ src/App.js
@@ -11,13 +11,13 @@ import UserProfile from "./pages/UserProfile";
 import Signup from "./pages/Signup";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBArXavmrZQ17slSBcoN5Ke60rIGQeuOdM",
-  authDomain: "exercise-five-e7ed8.firebaseapp.com",
-  databaseURL: "https://exercise-five-e7ed8.firebaseio.com",
-  projectId: "exercise-five-e7ed8",
-  storageBucket: "exercise-five-e7ed8.appspot.com",
-  messagingSenderId: "806229689552",
-  appId: "1:806229689552:web:ee55aebb11119943b3cad5"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECTID,
+  storageBucket: process.env.FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGINGSENDER,
+  appId: process.env.FIREBASE_APPID
 };
 
 function App() {
